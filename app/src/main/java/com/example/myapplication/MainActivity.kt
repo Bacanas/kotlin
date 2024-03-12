39c0a046f3b4c979ff014a53b2d405cbbf5f116c
Mosiah <127303388+Mosiahxyz@users.noreply.github.com>
2024-03-12 10:15:33 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,41 @@
-package com.example.myapplication
+package com.example.myapplication // definindo um "container" para organizar o projeto, enfrenta conflito entre classes
 
+//importando a biblioteca essencial que herda funcionalidades essenciais para gerenciar as atividades
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 
+//A classe Bundle é usada para passar dados entre atividades em um aplicativo Android
+import android.os.Bundle
+
+//importanto elementos 
+import android.widget.Button //botão
+import android.widget.TextView //texto
+import android.widget.Toast //mensagem pop up
+
+//não é obrigatório, porém o Kotlin trabalha melhor quando orientamos ele a objetos
+
+//criando a classe principal do nosso códio e herdamos as funcionalidades de AppCompat
 class MainActivity : AppCompatActivity() {
+
+    //definindo as váriaveis a serem manipuladas e armazenadas (elas serão privadas)
     private var pontuacaoTimeA: Int = 0
     private var pontuacaoTimeB: Int = 0
 
-    private lateinit var pTimeA: TextView
+    //essas váriaveis vão servir para mostrar na tela o saldo das pntuações
+    private lateinit var pTimeA: TextView// assim como Ts o Kotlin é fortemente tipado
     private lateinit var pTimeB: TextView
 
-
+    //vamos chamar o override pois queremos sobrescrever uma função do pai
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //nós chamamos  o método pai para garantir que ele vai ser usado aqui
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //esse código retorna a referência visiual, para que nós possamos interagir com o Text View
         pTimeA = findViewById(R.id.placarTimeA)
         pTimeB = findViewById(R.id.placarTimeB)
 
+        //criando os botões da pontuação val == const
         val bTresPontosTimeA: Button = findViewById(R.id.tresPontosA)
         val bDoisPontosTimeA: Button = findViewById(R.id.doisPontosA)
         val bTLivreTimeA: Button = findViewById(R.id.tiroLivreA)
